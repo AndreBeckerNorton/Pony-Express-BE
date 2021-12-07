bc21c3526b0cc993fb9f9d2eaa11736d26ad8b9f
Andre Becker Norton <andrenorton2022@u.northwestern.edu>
2021-12-06 23:21:26 -0800

    MongoDB stuff

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 // console.log ("Hello World")
 
+require('dotenv').config();
+var http = require('http');
+
 const express = require('express');
 const { customAlphabet } = require('nanoid');
 const alphabet = '0123456789abcdef';
 const nanoid = customAlphabet(alphabet, 24);
 const app = express();
 app.use(express.json())
+var server = http.createServer(app);
+const port = process.env.PORT; 
+const { mongoConnect } = require('./util/database'); 
 
 app.get('/', (req, res) => {
     res
@@ -35,3 +41,7 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
+
+mongoConnect(() => {
+  //server.listen(port, () => { console.log('We are live on ' + port); });
+}) 
